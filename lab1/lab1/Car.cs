e03d8c5a83eb119a50cf019e555d823f2099a8fb
Бадягаев Н.В. ИСЭбд-21 <40666743+Berlof@users.noreply.github.com>
2018-12-14 20:35:58 +0300

    Update Car.cs

--- lab1/lab1/Car.cs
+++ lab1/lab1/Car.cs
@@ -68,37 +68,13 @@ namespace WindowsFormsCars
             g.DrawRectangle(pen, _startPosX, _startPosY, 50, 40);
             g.DrawEllipse(pen, _startPosX - 0, _startPosY + 40, 100, 25);
             g.DrawRectangle(pen, _startPosX + 50, _startPosY + 15, 20, 25);
-
-
-
-
-            //g.DrawEllipse(pen, _startPosX, _startPosY, 140, 40);
-            //g.DrawEllipse(pen, _startPosX, _startPosY + 30, 20, 20);
-            //g.DrawEllipse(pen, _startPosX + 70, _startPosY, 20, 20);
-            //g.DrawEllipse(pen, _startPosX + 70, _startPosY + 30, 20, 20);
-            //g.DrawRectangle(pen, _startPosX - 1, _startPosY + 10, 10, 30);
-            //g.DrawRectangle(pen, _startPosX + 80, _startPosY + 10, 10, 30);
-            //g.DrawRectangle(pen, _startPosX + 10, _startPosY - 1, 70, 52);
-            //задние фары
-
-
-            // g.FillEllipse(brRed, _startPosX, _startPosY, 20, 20);
-            //  g.FillEllipse(brRed, _startPosX, _startPosY + 30, 20, 20);
-            //передние фары
             Brush brYellow = new SolidBrush(Color.Yellow);
-
             g.FillRectangle(brYellow, _startPosX, _startPosY, 50, 40);
             g.FillRectangle(brYellow, _startPosX + 50, _startPosY + 15, 20, 25);
             Brush brBlue = new SolidBrush(Color.Turquoise);
             g.FillRectangle(brBlue, _startPosX, _startPosY, 50, 15);
             Brush brBlack = new SolidBrush(Color.Black);
             g.FillEllipse(brBlack, _startPosX - 0, _startPosY + 40, 100, 25);
-
-            //кузов
-            // Brush br = new SolidBrush(MainColor);
-            // g.FillRectangle(br, _startPosX, _startPosY + 10, 10, 30);
-            //  g.FillRectangle(br, _startPosX + 80, _startPosY + 10, 10, 30);
-            // g.FillRectangle(br, _startPosX + 10, _startPosY, 70, 50);
         }
     }
-}
+}
