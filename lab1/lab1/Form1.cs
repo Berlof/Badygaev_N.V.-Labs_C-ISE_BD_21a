b032db8c5ef9c07503855073689582715ac98db1
Бадягаев Н.В. ИСЭбд-21 <40666743+Berlof@users.noreply.github.com>
2018-12-14 21:30:00 +0300

    Update Form1.cs

--- lab1/lab1/Form1.cs
+++ lab1/lab1/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using WindowsFormsCars;
 
@@ -14,10 +8,7 @@ namespace lab1
     public partial class Form1 : Form
     {
 
-
         private Itrandport car;
-
-
 
         public Form1()
         {
@@ -31,11 +22,6 @@ namespace lab1
             car.DrawCar(gr);
             pictureBoxBulldozer.Image = bmp;
         }
-        /// <summary>
-        /// Обработка нажатия кнопки "Создать"
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void buttonCreate_Click(object sender, EventArgs e)
         {
             Random rnd = new Random();
@@ -81,8 +67,4 @@ namespace lab1
             Draw();
         }
     }
-
-
-
-
 }
