738a7c3a26979da63e5b0b2d324e99f8de2bb63e
Бадягаев Н.В. ИСЭбд-21 <40666743+Berlof@users.noreply.github.com>
2019-01-11 18:02:59 +0400

    Update Program.cs

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -17,7 +17,7 @@ namespace lab1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormAuto());
         }
     }
 }
