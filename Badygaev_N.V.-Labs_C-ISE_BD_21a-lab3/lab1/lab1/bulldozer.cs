a98b9b886f5609918355b78e08aa09ddd5f73586
Бадягаев Н.В. ИСЭбд-21 <40666743+Berlof@users.noreply.github.com>
2019-01-11 22:39:06 +0300

    Add files via upload

---
+++ Badygaev_N.V.-Labs_C-ISE_BD_21a-lab3/lab1/lab1/bulldozer.cs
@@ -0,0 +1,57 @@
+using System.Drawing;
+using WindowsFormsCars;
+
+namespace lab1
+{
+    class bulldozer : Car
+    {
+        public Color DopColor { private set; get; }
+        /// <summary>
+        /// Признак наличия переднего ковша
+        /// </summary>
+        public bool FrontLadle { private set; get; }
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="maxSpeed">Максимальная скорость</param>
+        /// <param name="weight">Вес автомобиля</param>
+        /// <param name="mainColor">Основной цвет кузова</param>
+        /// <param name="dopColor">Дополнительный цвет</param>
+        /// <param name="frontLadle">Признак наличия переднего ковша</param>
+        /// <param name="sideSpoiler">Признак наличия боковых спойлеров</param>
+        /// <param name="backSpoiler">Признак наличия заднего спойлера</param>
+        public bulldozer(int maxSpeed, float weight, Color mainColor, Color dopColor, bool frontLadle) : base(maxSpeed, weight, mainColor)
+        {
+            DopColor = dopColor;
+            FrontLadle = frontLadle;
+
+        }
+        /// <summary>
+        /// Установка позиции автомобиля
+        /// </summary>
+        /// <param name="x">Координата X</param>
+        /// <param name="y">Координата Y</param>
+        /// <param name="width">Ширина картинки</param>
+        /// <param name="height">Высота картинки</param>
+        public override void DrawCar(Graphics g)
+        {
+            // отрисуем сперва передний спойлер автомобиля (чтобы потом отрисовк автомобиля на него "легла")
+            if (FrontLadle)
+            {
+                // Brush spoiler = new SolidBrush(DopColor);
+                Pen spoiler = new Pen(DopColor);
+                g.DrawLine(spoiler, _startPosX + 70, _startPosY + 20, _startPosX + 95, _startPosY + 60);
+                g.DrawLine(spoiler, _startPosX + 95, _startPosY + 60, _startPosX + 105, _startPosY + 60);
+                g.DrawLine(spoiler, _startPosX + 70, _startPosY + 20, _startPosX + 105, _startPosY + 60);
+                int n = 0;
+                while (n < 20)
+                {
+                    n++;
+                    g.DrawLine(spoiler, _startPosX + 70, _startPosY + 20, _startPosX + 95 + n, _startPosY + 60);
+                }
+            }
+            base.DrawCar(g);
+        }
+    }
+}
+
