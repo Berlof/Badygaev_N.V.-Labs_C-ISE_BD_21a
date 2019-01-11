d42f975d6e3385ad3dc18298fa02976383404154
Бадягаев Н.В. ИСЭбд-21 <40666743+Berlof@users.noreply.github.com>
2019-01-11 22:36:24 +0300

    Add files via upload

---
+++ Badygaev_N.V.-Labs_C-ISE_BD_21a-lab1/lab1/Badygaev_N.V.-Labs_C-ISE_BD_21a-lab2/lab1/lab1/Car.cs
@@ -0,0 +1,77 @@
+﻿using lab1;
+using System.Drawing;
+
+
+namespace WindowsFormsCars
+{
+    public class Car : Vehicle
+    {
+        private const int carWidth = 130;
+        /// <summary>
+        /// Ширина отрисовки автомобиля
+        /// </summary>
+        private const int carHeight = 80;
+        /// <summary>
+        /// Максимальная скорость
+        /// </summary>
+        public Car(int maxSpeed, float weight, Color mainColor)
+        {
+            MaxSpeed = maxSpeed;
+            Weight = weight;
+            MainColor = mainColor;
+        }
+        public override void MoveTransport(Direction direction)
+        {
+            float step = MaxSpeed * 100 / Weight;
+            switch (direction)
+            {
+                // вправо
+                case Direction.Right:
+                    if (_startPosX + step < _pictureWidth - carWidth)
+                    {
+                        _startPosX += step;
+                    }
+                    break;
+                //влево
+                case Direction.Left:
+                    if (_startPosX - step > 0)
+                    {
+                        _startPosX -= step;
+                    }
+                    break;
+                //вверх
+                case Direction.Up:
+                    if (_startPosY - step > 0)
+                    {
+                        _startPosY -= step;
+                    }
+                    break;
+                //вниз
+                case Direction.Down:
+                    if (_startPosY + step < _pictureHeight - carHeight)
+                    {
+                        _startPosY += step;
+                    }
+                    break;
+            }
+        }
+        public override void DrawCar(Graphics g)
+        {
+            Pen pen = new Pen(Color.Black);
+            //границы автомобиля
+            // теперь отрисуем основной кузов автомобиля
+            //границы автомобиля
+            g.DrawRectangle(pen, _startPosX, _startPosY, 50, 15);
+            g.DrawRectangle(pen, _startPosX, _startPosY, 50, 40);
+            g.DrawEllipse(pen, _startPosX - 10, _startPosY + 40, 100, 25);
+            g.DrawRectangle(pen, _startPosX + 50, _startPosY + 15, 20, 25);
+            Brush brYellow = new SolidBrush(Color.Yellow);
+            g.FillRectangle(brYellow, _startPosX, _startPosY, 50, 40);
+            g.FillRectangle(brYellow, _startPosX + 50, _startPosY + 15, 20, 25);
+            Brush brBlue = new SolidBrush(Color.Turquoise);
+            g.FillRectangle(brBlue, _startPosX, _startPosY, 50, 15);
+            Brush brBlack = new SolidBrush(Color.Black);
+            g.FillEllipse(brBlack, _startPosX - 10, _startPosY + 40, 100, 25);
+        }
+    }
+}
---
+++ Badygaev_N.V.-Labs_C-ISE_BD_21a-lab1/lab1/Badygaev_N.V.-Labs_C-ISE_BD_21a-lab2/lab1/lab1/Vehicle.cs
@@ -0,0 +1,51 @@
+﻿using lab1;
+using System.Drawing;
+
+namespace WindowsFormsCars
+{
+    public abstract class Vehicle : ITrandport
+    {
+        /// <summary>       
+        /// Левая координата отрисовки автомобиля  
+        /// </summary>      
+        protected float _startPosX;
+        /// <summary>       
+        /// Правая кооридната отрисовки автомобиля    
+        /// </summary>     
+        protected float _startPosY;
+        /// <summary>       
+        /// Ширина окна отрисовки
+        /// </summary>
+        protected int _pictureWidth;
+        //Высота окна отрисовки      
+        protected int _pictureHeight;
+        /// <summary>  
+        /// Максимальная скорость    
+        /// /// </summary>      
+        public int MaxSpeed { protected set; get; }
+        /// <summary>       
+        /// Вес автомобиля
+        /// </summary>      
+        public float Weight { protected set; get; }
+
+        public float Height { protected set; get; }
+        /// <summary>      
+        /// Основной цвет кузова    
+        /// </summary> 
+        public Color MainColor { protected set; get; }
+
+        public void SetPosition(int x, int y, int width, int height)
+        {
+            _startPosX = x;
+            _startPosY = y;
+            _pictureWidth = width;
+            _pictureHeight = height;
+        }
+
+        public abstract void DrawCar(Graphics g);
+
+        public abstract void MoveTransport(Direction direction);
+
+
+    }
+}
