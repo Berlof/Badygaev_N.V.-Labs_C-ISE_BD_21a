eaae3dcde7ae8cb37a068e9640cd37dccfac71cc
Бадягаев Н.В. ИСЭбд-21 <40666743+Berlof@users.noreply.github.com>
2019-01-12 00:09:06 +0400

    Add files via upload

---
+++ FormParking.cs
@@ -0,0 +1,187 @@
+﻿using lab1;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsCars
+
+{
+    public partial class FormParking : Form
+    {/// <summary>
+     /// Объект от класса многоуровневой парковки
+     /// </summary>
+        MultiLevelParking parking;
+        /// <summary>
+        /// Форма для добавления
+        /// </summary>
+        FormCarConfig form;
+        /// <summary>
+        /// Количество уровней-парковок
+        /// </summary>
+        /// 
+        private const int countLevel = 5;
+        /// <summary>
+        /// Логгер
+        /// </summary>
+        public FormParking()
+        {
+            InitializeComponent();
+            parking = new MultiLevelParking(countLevel, pictureBoxParking.Width, pictureBoxParking.Height);
+            //заполнение listBox
+            for (int i = 0; i < countLevel; i++)
+            {
+                listBoxLevels.Items.Add("Уровень " + (i + 1));
+            }
+            listBoxLevels.SelectedIndex = 0;
+        }
+        private void Draw()
+        {
+            if (listBoxLevels.SelectedIndex > -1)
+            {
+                Bitmap bmp = new Bitmap(pictureBoxParking.Width, pictureBoxParking.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                parking[listBoxLevels.SelectedIndex].Draw(gr);
+                pictureBoxParking.Image = bmp;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listBoxLevels.SelectedIndex > -1)
+            {
+                ColorDialog dialog = new ColorDialog();
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    var car = new Car(100, 1000, dialog.Color);
+                    int place = parking[listBoxLevels.SelectedIndex] + car;
+                    if (place == -1)
+                    {
+                        MessageBox.Show("Нет свободных мест", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    Draw();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBoxLevels.SelectedIndex > -1)
+            {
+                ColorDialog dialog = new ColorDialog();
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    ColorDialog dialogDop = new ColorDialog();
+                    if (dialogDop.ShowDialog() == DialogResult.OK)
+                    {
+                        var car = new bulldozer(100, 1000, dialog.Color, dialogDop.Color, true);
+                        int place = parking[listBoxLevels.SelectedIndex] + car;
+                        if (place == -1)
+                        {
+                            MessageBox.Show("Нет свободных мест", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        Draw();
+                    }
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBoxLevels.SelectedIndex > -1)
+            {
+                if (maskedTextBox.Text != "")
+                {
+                    var car = parking[listBoxLevels.SelectedIndex] -
+                   Convert.ToInt32(maskedTextBox.Text);
+                    if (car != null)
+                    {
+                        Bitmap bmp = new Bitmap(pictureBoxTakeCar.Width, pictureBoxTakeCar.Height);
+                        Graphics gr = Graphics.FromImage(bmp);
+                        car.SetPosition(5, 5, pictureBoxTakeCar.Width, pictureBoxTakeCar.Height);
+                        car.DrawCar(gr);
+                        pictureBoxTakeCar.Image = bmp;
+                    }
+                    else
+                    {
+                        Bitmap bmp = new Bitmap(pictureBoxTakeCar.Width, pictureBoxTakeCar.Height);
+                        pictureBoxTakeCar.Image = bmp;
+                    }
+                    Draw();
+                }
+            }
+        }
+        /// <summary>
+        /// Метод обработки выбора элемента на listBoxs
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listBoxLevels_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Draw();
+        }
+        private void listBoxLevels_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            form = new FormCarConfig();
+            form.AddEvent(AddCar);
+            form.Show();
+        }
+        private void AddCar(Itrandport car)
+        {
+            if (car != null && listBoxLevels.SelectedIndex > -1)
+            {
+                int place = parking[listBoxLevels.SelectedIndex] + car;
+                if (place > -1)
+                {
+                    Draw();
+                }
+                else
+                {
+                    MessageBox.Show("Машину не удалось поставить");
+                }
+            }
+        }
+        /// <summary>
+        /// Обработка нажатия пункта меню "Сохранить"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (parking.SaveData(saveFileDialog.FileName))
+                {
+                    MessageBox.Show("Сохранение прошло успешно", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Не сохранилось", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        /// <summary>
+        /// Обработка нажатия пункта меню "Загрузить"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void загрузитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (parking.LoadData(openFileDialog.FileName))
+                {
+                    MessageBox.Show("Загрузили", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Не загрузили", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                Draw();
+            }
+        }
+    }
+}
---
+++ bulldozer.cs
@@ -0,0 +1,72 @@
+using System.Drawing;
+using WindowsFormsCars;
+
+namespace lab1
+{
+    class bulldozer : Car
+    {
+        public Color DopColor { private set; get; }
+        /// <summary>
+        /// Признак наличия переднего ковша
+        /// </summary>
+        public bool FrontLadle { private set; get; }
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="maxSpeed">Максимальная скорость</param>
+        /// <param name="weight">Вес автомобиля</param>
+        /// <param name="mainColor">Основной цвет кузова</param>
+        /// <param name="dopColor">Дополнительный цвет</param>
+        /// <param name="frontLadle">Признак наличия переднего ковша</param>
+        /// <param name="sideSpoiler">Признак наличия боковых спойлеров</param>
+        /// <param name="backSpoiler">Признак наличия заднего спойлера</param>
+        public bulldozer(int maxSpeed, float weight, Color mainColor, Color dopColor, bool  frontLadle) : base(maxSpeed, weight, mainColor)
+        {
+            DopColor = dopColor;
+            FrontLadle = frontLadle;
+        }
+        public bulldozer(string info) : base(info)
+        {
+            string[] strs = info.Split(';');
+            if (strs.Length == 5)
+            {
+                MaxSpeed = Convert.ToInt32(strs[0]);
+                Weight = Convert.ToInt32(strs[1]);
+                MainColor = Color.FromName(strs[2]);
+                DopColor = Color.FromName(strs[3]);
+                FrontLadle = Convert.ToBoolean(strs[4]);
+            }
+        }
+        public override void DrawCar(Graphics g)
+        {
+            // отрисуем сперва передний спойлер автомобиля (чтобы потом отрисовк автомобиля на него "легла")
+            if (FrontLadle)
+            {
+                // Brush spoiler = new SolidBrush(DopColor);
+                Pen spoiler = new Pen(DopColor);
+                g.DrawLine(spoiler, _startPosX + 70, _startPosY + 20, _startPosX + 95, _startPosY + 60);
+                g.DrawLine(spoiler, _startPosX + 95, _startPosY + 60, _startPosX + 105, _startPosY + 60);
+                g.DrawLine(spoiler, _startPosX + 70, _startPosY + 20, _startPosX + 105, _startPosY + 60);
+                int n = 0;
+                while (n < 20)
+                {
+                    n++;
+                    g.DrawLine(spoiler, _startPosX + 70, _startPosY + 20, _startPosX + 95 + n, _startPosY + 60);
+                }
+
+
+            }
+            base.DrawCar(g);
+        }
+        public void SetDopColor(Color color)
+        {
+            DopColor = color;
+        }
+        
+        public override string ToString()
+        {
+            return base.ToString() + ";" + DopColor.Name + ";" + FrontLadle;
+        }
+    }
+}
+
