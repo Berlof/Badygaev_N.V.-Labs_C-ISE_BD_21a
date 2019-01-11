ba14d2b0643394eefe3c18018df28115e0a6d292
Бадягаев Н.В. ИСЭбд-21 <40666743+Berlof@users.noreply.github.com>
2019-01-12 00:04:16 +0400

    Add files via upload

---
+++ FormParking.cs
@@ -0,0 +1,82 @@
+﻿using lab1;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+namespace WindowsFormsCars
+{
+    public partial class FormParking : Form
+    {
+        /// <summary>
+        /// Объект от класса-парковки
+        /// </summary>
+        Parking<Itrandport> parking;
+        public FormParking()
+        {
+            InitializeComponent();
+            parking = new Parking<Itrandport>(20, pictureBoxParking.Width,
+           pictureBoxParking.Height);
+            Draw();
+        }
+        /// <summary>
+        /// Метод отрисовки парковки
+        /// </summary>
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBoxParking.Width, pictureBoxParking.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            parking.Draw(gr);
+            pictureBoxParking.Image = bmp;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                var car = new Car(100, 1000, dialog.Color);
+                int place = parking + car;
+                Draw();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                ColorDialog dialogDop = new ColorDialog();
+                if (dialogDop.ShowDialog() == DialogResult.OK)
+                {
+                    var car = new bulldozer(100, 1000, dialog.Color, dialogDop.Color,
+                   true);
+                    int place = parking + car;
+                    Draw();
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (maskedTextBox.Text != "")
+            {
+                var car = parking - Convert.ToInt32(maskedTextBox.Text);
+                if (car != null)
+                {
+                    Bitmap bmp = new Bitmap(pictureBoxTakeCar.Width,
+                   pictureBoxTakeCar.Height);
+                    Graphics gr = Graphics.FromImage(bmp);
+                    car.SetPosition(5, 5, pictureBoxTakeCar.Width,
+                   pictureBoxTakeCar.Height);
+                    car.DrawCar(gr);
+                    pictureBoxTakeCar.Image = bmp;
+                }
+                else
+                {
+                    Bitmap bmp = new Bitmap(pictureBoxTakeCar.Width,
+                   pictureBoxTakeCar.Height);
+                    pictureBoxTakeCar.Image = bmp;
+                }
+                Draw();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using WindowsFormsCars;
+
+namespace lab1
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormParking());
+        }
+    }
+}
