6a44947d662596e5041f17f3311e497e42b0c8a0
Бадягаев Н.В. ИСЭбд-21 <40666743+Berlof@users.noreply.github.com>
2019-01-16 19:24:10 +0300

    Добавлена лаба3

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Labs
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormAuto());
+            Application.Run(new FormParking());
         }
     }
 }
