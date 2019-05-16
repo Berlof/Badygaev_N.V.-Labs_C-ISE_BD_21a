3aec6347f7b233357444c4d6bbba55b2a273d4bc
Бадягаев Н.В. ИСЭбд-21 <40666743+Berlof@users.noreply.github.com>
2019-05-16 23:34:22 +0400

    1

--- AbstractMotorFactoryModel/Engine.cs
+++ AbstractMotorFactoryModel/Engine.cs
@@ -9,7 +9,9 @@ namespace AbstractMotorFactoryModel
     public class Engine
     {
         public int Id { get; set; }
+     
         public string EngineName { get; set; }
+        
         public decimal Cost { get; set; }
     }
 }
