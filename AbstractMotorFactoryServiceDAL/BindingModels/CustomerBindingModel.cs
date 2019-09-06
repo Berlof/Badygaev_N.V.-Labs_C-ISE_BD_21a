4b35453739e1da489316130d0950d4850b0f73aa
Бадягаев Н.В. ИСЭбд-21 <40666743+Berlof@users.noreply.github.com>
2019-09-06 20:07:45 +0300

    Add files via upload

--- AbstractMotorFactoryServiceDAL/BindingModels/CustomerBindingModel.cs
+++ AbstractMotorFactoryServiceDAL/BindingModels/CustomerBindingModel.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel;
+
 namespace AbstractMotorFactoryServiceDAL.BindingModels
 {
     public class CustomerBindingModel
     {
         public int Id { get; set; }
 
+        [DisplayName("ФИО покупателя")]
         public string CustomerFIO { get; set; }
     }
 }
